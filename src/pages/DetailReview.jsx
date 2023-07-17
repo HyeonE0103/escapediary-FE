2695a749a68fdafab1b1df7a1c90f35e0700c61f
HyeonE <mag30102000@gmail.com>
2023-07-18 00:08:40 +0900

    style: 상세 페이지에 맞는 반응형 UI 구현

--- src/pages/DetailReview.jsx
+++ src/pages/DetailReview.jsx
@@ -1,133 +1,180 @@
-import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Header from "../components/common/Header";
 import { styled } from "styled-components";
+import { useParams } from "react-router-dom";
 import Button from "../components/common/Button";
 import useNavigation from "../hooks/useNavigation";
-import axios from "axios";
 
 const DetailReview = () => {
-  const { goBack } = useNavigation();
-  const [user, setUser] = useState(false);
   const [data, setData] = useState(null);
+  const [user, setUser] = useState(false);
   const postId = useParams();
+  const { goBack } = useNavigation();
 
   useEffect(() => {
     const api = async () => {
-      const api = process.env.REACT_APP_URL;
+      const url = process.env.REACT_APP_URL;
       try {
-        const apiData = await axios.get(api);
-        setData(apiData.data.posts);
-        console.log(apiData.data.posts);
+        const apiData = await axios.get(url);
+        const postData = apiData.data.posts.filter(
+          (item) => item.postId === Number(postId.postid)
+        );
+        setData(postData[0]);
       } catch (e) {
         console.log(e);
       }
     };
     api();
   }, []);
-
   return (
     <DetailWrap>
-      <DetailContainer>
-        {data && data
-          .filter((item => item.postId == postId.postid))
-          .map((item) => {
-            return (
-              <>
-                <DetailHeader>
-                  <div className="backIcon" onClick={goBack}>
-                    ←
-                  </div>
-                  {/* icon 처리 */}
-                  <h1>{item.title}</h1>
-                  <div className="detailStar">{`${'⭐'.repeat(item.star)}`}</div>
-                </DetailHeader>
-                <DetailBody>
-                  <div className="detailContent">{item.content}</div>
-                  <div className="detailDate">{item.createdAt.slice(0, 10)}</div>
-                </DetailBody>
-              </>
-            )
-          })}
-        {user && (
-          <DetailBodyFooter>
-            <Button color={"black"} size={"medium"}>
-              수정
-            </Button>
-            <Button color={"black"} size={"medium"}>
-              삭제
-            </Button>
-          </DetailBodyFooter>
-        )}
-      </DetailContainer>
+      <Header />
+      {data && (
+        <DetailBody>
+          <InformationSection>
+            <div className="backIcon" onClick={goBack}>
+              ←
+            </div>
+            <div className="informationContent">
+              {user && <div>userID</div>}
+              <div>{"⭐".repeat(data.star)}</div>
+              <div>{data.createdAt.slice(0, 10)}</div>
+            </div>
+            {user && (
+              <div className="userButton">
+                <Button color={"white"} size={"small"}>
+                  수정
+                </Button>
+                <Button color={"white"} size={"small"}>
+                  삭제
+                </Button>
+              </div>
+            )}
+          </InformationSection>
+          <ContentSection>
+            <div className="contentTitle">
+              <div className="backIcon" onClick={goBack}>
+                ←
+              </div>
+              <div>{data.title}</div>
+            </div>
+            <div className="contentContent">{data.content}</div>
+          </ContentSection>
+        </DetailBody>
+      )}
     </DetailWrap>
   );
 };
-
 const DetailWrap = styled.div`
   width: 100%;
   height: 100vh;
   display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-const DetailContainer = styled.div`
-  width: 40rem;
-  height: 50rem;
-  display: flex;
   flex-direction: column;
-  border: 1px solid #d8d8d8;
-  position: relative;
-  padding: 0 2rem;
-  box-sizing: border-box;
-`;
-const DetailHeader = styled.div`
-  flex: 2;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  .backIcon {
-    position: absolute;
-    top: 2rem;
-    left: 2rem;
-    font-size: 2rem;
-    font-weight: bold;
-    cursor: pointer;
-  }
-  h1 {
-    font-size: 2rem;
-    font-weight: bold;
-    word-break: break-all;
-    overflow-wrap: break-word;
-    text-align: center;
-  }
-  .detailStar {
-    padding-top: 1.5rem;
-    font-size: 1.2rem;
-  }
 `;
 const DetailBody = styled.div`
+  box-sizing: border-box;
+  padding: 3%;
+  display: flex;
+  width: 100%;
+  height: 100%;
+  @media (max-width: 480px) {
+    flex-direction: column;
+  }
+`;
+const InformationSection = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  background-color: #2e2e2e;
+  color: white;
+  font-size: 1.5rem;
+  box-sizing: border-box;
+  padding: 3%;
+  .backIcon {
+    font-weight: bold;
+    font-size: 2rem;
+    margin-left: 0;
+  }
+  .informationContent {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    gap: 3%;
+  }
+  .userButton {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+  @media (max-width: 480px) {
+    order: 2;
+    font-size: 1rem;
+    flex-direction: row;
+    justify-content: space-evenly;
+    .backIcon {
+      display: none;
+    }
+    .informationContent {
+      gap: 10%;
+    }
+    .userButton {
+      flex-direction: column;
+      justify-content: space-evenly;
+    }
+  }
+`;
+const ContentSection = styled.div`
   flex: 5;
   display: flex;
   flex-direction: column;
-  .detailContent {
+  .contentTitle {
     flex: 1;
-    padding: 0.5rem;
-    border: 1px solid #d8d8d8;
-    overflow: auto;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #1c1c1c;
+    font-size: 2rem;
+    color: white;
+    font-weight: bold;
+    word-break: break-all;
+    overflow-wrap: break-word;
+    padding: 5%;
+    .backIcon {
+      display: none;
+    }
+  }
+  .contentContent {
+    flex: 3;
+    margin: 2% 0 0 2%;
+    padding: 2%;
+    box-sizing: border-box;
+    border: 1px #d8d8d8 solid;
+    font-size: 1.2rem;
     word-break: break-all;
     overflow-wrap: break-word;
   }
-  .detailDate {
-    padding: 1rem 0;
-    font-weight: bold;
-    margin-left: auto;
+  @media (max-width: 480px) {
+    flex: 5;
+    position: relative;
+    .contentTitle {
+      font-size: 1.3rem;
+      flex-direction: column;
+      .backIcon {
+        display: unset;
+        font-size: 1.5rem;
+        position: absolute;
+        top: 0;
+        left: 0;
+        margin: 2%;
+      }
+    }
+    .contentContent {
+      margin: 2% 0 2% 0;
+    }
   }
-`;
-const DetailBodyFooter = styled.div`
-  flex: 2;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
 `;
 export default DetailReview;
