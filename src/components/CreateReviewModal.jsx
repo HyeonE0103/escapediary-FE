6301dd1199e1deb1c9cbde291a8ea4548d4ef967
gyurison7 <orangetree401@gmail.com>
2023-07-20 04:12:42 +0900

    test: 게시글 수정 테스트

--- src/components/CreateReviewModal.jsx
+++ src/components/CreateReviewModal.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { styled } from "styled-components";
 import Button from "./common/Button";
 import axios from "axios";
