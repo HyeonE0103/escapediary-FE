7b6c1f9e70a888be0d84a68d12af33d60e34829a
HyeonE <mag30102000@gmail.com>
2023-07-20 03:07:17 +0900

    test: id와 상세조회

--- src/pages/DetailReview.jsx
+++ src/pages/DetailReview.jsx
@@ -28,7 +28,7 @@ const DetailReview = () => {
     api();
   }, []);
   console.log("상세 post 조회", data);
-  console.log("상세 user id 조회", user.id);
+  user !== null && console.log("상세 user id 조회", user.id);
   return (
     <DetailWrap>
       <Header />
@@ -43,7 +43,7 @@ const DetailReview = () => {
               <div>{"⭐".repeat(data.star)}</div>
               <div>{data.createdAt.slice(0, 10)}</div>
             </div>
-            {user === data.id && (
+            {user !== null && user.id === data.id && (
               <div className="userButton">
                 <Button color={"white"} size={"small"}>
                   수정
