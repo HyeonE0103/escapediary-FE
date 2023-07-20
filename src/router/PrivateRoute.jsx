e2d9af454d9bfc7d7d30fa6bc4356659fca34ac3
HyeonE <mag30102000@gmail.com>
2023-07-20 13:40:00 +0900

    test: 비정상적인 접근

--- src/router/PrivateRoute.jsx
+++ src/router/PrivateRoute.jsx
@@ -5,11 +5,13 @@ import { Outlet } from "react-router-dom";
 export const PrivateRoute = () => {
   const isUser = useSelector((state) => state.userData);
 
-  return isUser ? NotUser() : <Outlet />;
+  return isUser ? <NotUser /> : <Outlet />;
 };
 
 const NotUser = () => {
   const { goToPath } = useNavigation();
+
   alert("이미 로그인되어 있습니다");
   goToPath("/");
+  return null;
 };
